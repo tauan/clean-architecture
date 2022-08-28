37a08977364d8c1030c7ebc10250d6f4e1c3e35b
Tauan Gabriel <tokou_cavalcanti@hotmail.com>
2022-08-28 07:36:37 -0300

    chore: configurando mongo db in memory

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
-    '!<rootDir>/src/infra/db/**/*',
-    '<rootDir>/src/main/factories/validation/**/*',
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
-  }
 }
  
