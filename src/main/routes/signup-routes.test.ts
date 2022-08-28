fdafa3321f6578e9cf1b261fd7e8f1dbe3a9582f
Tauan Gabriel <tokou_cavalcanti@hotmail.com>
2022-08-28 20:38:12 -0300

    feat: connect to mongoDb before query

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,5 +1,19 @@
 import request from "supertest";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL);
+});
+
+afterAll(async () => {
+  await MongoHelper.disconnect();
+});
+
+beforeEach(async () => {
+  const accountsCollection = MongoHelper.getCollection("accounts");
+  await accountsCollection.deleteMany({});
+});
 
 describe("Signup routes", () => {
   test("Should return an accountn on success", async () => {
