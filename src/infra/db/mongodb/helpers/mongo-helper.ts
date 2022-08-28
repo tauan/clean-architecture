d3181e91f72f052a9fadbcb91fc36511239ba5fe
Tauan Gabriel <tokou_cavalcanti@hotmail.com>
2022-08-28 07:37:35 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,15 @@
+import { MongoClient, Collection } from "mongodb";
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect(url: string) {
+    this.client = await MongoClient.connect(process.env.MONGO_URL);
+  },
+  async disconnect() {
+    await this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
+  },
+};
