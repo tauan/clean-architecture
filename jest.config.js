a9ec0c5792b1e4c377cc69ba43d7a7f4a04bfced
Tauan Gabriel <tokou_cavalcanti@hotmail.com>
2022-08-28 22:47:20 -0300

    refactor: remove exports into coveragfe

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
